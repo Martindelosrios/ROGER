c9fd2cff68caf30dda42743563f06768868467ac
Debian <debian@testcosmo1.novalocal>
2019-11-26 17:08:43 +0000

    just the first commit, with the main folders and archives that we need. Also a simple checking function

---
+++ R/check.R
@@ -0,0 +1,13 @@
+#' a check function
+#'
+#' This function allows you to ....
+#' @param n
+#' @keywords check
+#' @export
+#' @examples
+#' check(n)
+check <- function(n){
+  for(i in 1:n){
+    print('hola')
+  }
+}
